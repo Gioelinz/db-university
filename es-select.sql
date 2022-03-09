51ba574b9203cb253b9f9385c9a1f21cfd0bebb1
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-09 15:38:16 +0100

    add last excercise

--- es-select.sql
+++ es-select.sql
@@ -21,3 +21,27 @@ WHERE YEAR(`date_of_birth`) < 1992;
 SELECT * 
 FROM `courses`
 WHERE `period` = 'I semestre' AND `year` = 1;
+
+-- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+SELECT * 
+FROM `exams`
+WHERE HOUR(`hour`) > 14
+AND `date` = '2020-06-20';
+
+-- Selezionare tutti i corsi di laurea magistrale (38)
+
+SELECT * 
+FROM `degrees`
+WHERE `name` LIKE 'Corso di Laurea Magistrale%';
+
+-- Da quanti dipartimenti è composta l'università? (12)
+
+SELECT COUNT(*) AS `all_departments`
+FROM `departments`;
+
+-- . Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT COUNT(*) AS `teachers_without_phone`
+FROM `teachers`
+WHERE `phone` IS NOT NULL;
