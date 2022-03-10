939284866862abb631c5ae3376b8aecd9f5a48e3
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-10 15:37:18 +0100

    add es 3-4

--- es-join.sql
+++ es-join.sql
@@ -13,3 +13,26 @@ FROM `departments`
 JOIN `degrees`
 ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+--Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.`name` AS 'course name', `teachers`.`id` AS 'teacher id'
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id`= `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44;
+
+--Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui 
+--sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`surname`, `students`.`name`, `degrees`.`name` AS 'Degree', `departments`.`name` AS 'Department name'
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+JOIN `students`
+ON `degrees`.`id` = `students`.`degree_id`
+ORDER BY `students`.`surname`, `students`.`name`;
+
+--Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
