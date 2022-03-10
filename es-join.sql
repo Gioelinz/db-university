1ff1024086e8035dd3f678eecf8f0c917e3a6590
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-10 15:17:44 +0100

    add first 2 es

--- es-join.sql
+++ es-join.sql
@@ -1,1 +1,15 @@
 -- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT `students`.*
+FROM `degrees` 
+JOIN `students` 
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+--Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT `degrees`.*
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
